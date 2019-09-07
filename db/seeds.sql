0fd77a9dfc1ee4d97e8b4a4b51359c31266b3fcc
Mike Smith <smith.mike@mac.com>
2019-09-07 15:22:18 -0400

    added db schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('McDouble');
+INSERT INTO burgers (burger_name, devour) VALUES ('Smokehouse', true);
+INSERT INTO burgers (burger_name, devour) VALUES ('Yeah', true);
+INSERT INTO burgers (burger_name, devour) VALUES ('Grind House', true);
+INSERT INTO burgers (burger_name) VALUES ('Baconator');
